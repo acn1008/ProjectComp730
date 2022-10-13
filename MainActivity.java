41a98facc4cb6f2b6f5d55676689c55709353f2b
acn1008 <42759494+acn1008@users.noreply.github.com>
2022-10-12 22:04:26 -0400

    Starting code from temperature converter
    Starting code from temperature converter. Will probably need to implement class such as from the Planets Lav

---
+++ MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.temperatureconverter;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button cToF, fToC;
+    private TextView result;
+    private EditText enterTemp;
+    double result0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cToF = findViewById(R.id.cToF);
+        fToC = findViewById(R.id.fToC);
+        result = findViewById(R.id.result);
+        enterTemp = findViewById(R.id.enterTemp);
+
+        cToF.setOnClickListener(v -> {
+            double temp = Double.parseDouble(enterTemp.getText().toString());
+            result0 = (temp * 1.8) + 32;
+            result.setText(String.valueOf(result0));
+        });
+
+        fToC.setOnClickListener(v -> {
+            double temp = Double.parseDouble(enterTemp.getText().toString());
+            result0 = (temp - 32) / 1.8;
+            result.setText(String.valueOf(result0));
+        });
+
+    }
+
+}
+
+
