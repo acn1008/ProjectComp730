f03f2bbbc7b368b9c33cc2575ed8bb1024546089
acn1008 <42759494+acn1008@users.noreply.github.com>
2022-11-28 20:46:54 -0500

    Add files via upload
    Adding completed ConverterClass file with Kelvin conversions added

--- ConverterClass.java
+++ ConverterClass.java
@@ -3,31 +3,26 @@ package com.example.tempconverterbothfarenheitandcelsius;
 public class ConverterClass {
     //celsius to fahrenheit calculation
     public double c2f_fn(double celsius) {
-        return (celsius*1.8)+32;    
+        return (celsius*1.8)+32;
     }
-    
     //fahrenheit to celsius calculation
     public double f2c_fn(double fahrenheit) {
         return ((fahrenheit-32)*5)/9;
     }
-    
+    //Calculation for Kelvin to Celsius
     public double k2c_fn(double kelvin) {
-        //Calculation for Kelvin to Celsius
         return (kelvin - 273.15);
     }
-
+    //Calculation for Kelvin to Fahrenheit
     public double k2f_fn(double kelvin) {
-        //Calculation for Kelvin to Fahrenheit
         return (double)((kelvin-273.15)*1.8+32);
     }
-
+    //Calculation for Fahrenheit to Kelvin
     public double f2k_fn(double fahrenheit) {
-        //Calculation for Fahrenheit to Kelvin
         return (double)((fahrenheit-32)*5/9+273.15);
     }
-
+    //Calculation for Celsius to Kelvin
     public double c2k_fn(double celsius) {
-        //Calculation for Celsius to Kelvin
         return (celsius+273.15);
     }
 }
